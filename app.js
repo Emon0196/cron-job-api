d81e3a633acb45066dd9a1cc76285d4f25d3f8d6
Emon0196 <mehedi.hasan.emon@g.bracu.ac.bd>
2024-12-03 10:19:37 +0600

    minor change in app.js

--- app.js
+++ app.js
@@ -38,3 +38,7 @@ app.use(errorHandler);
 // Server Initialization
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
